8a21c43350f096fa95be7fb2a974d09a27bb4c32
SantiagoLoaiza247 <128566879+darkfast247@users.noreply.github.com>
2024-04-25 13:45:42 -0500

    primer  commir

---
+++ PazYSalvoAPPCliente/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, SideMenuComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'PazYSalvoAPPCliente';
+}
